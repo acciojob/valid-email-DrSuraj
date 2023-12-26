307305fa21ea731c0aa6f7fee9541d43aed93d6e
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 15:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function validEmail(str) {
-  //your JS code here.
+function validEmail(email) {
+    // Regular expression for validating email addresses
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Test the email against the regex and return the result
+    return emailRegex.test(email);
 }
 
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// Examples
+console.log(validEmail('abc@example.com'));    // true
+console.log(validEmail('xyz@abc.com.in'));      // true
+console.log(validEmail('john.doe@com.'));       // false
+console.log(validEmail('@example.com'));        // false
+console.log(validEmail(''));                    // false
